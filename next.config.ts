3986df5d42f89eff375b60c8c27fa471e15d88b8
Minerafter9 <Minerafter9@gmail.com>
2025-09-21 04:54:23 +1000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["example.com"],
+  },
 };
 
 export default nextConfig;
